0e5ac5e07748ebda4a9a6015038b5b6a993fb474
Daniel Plazas <danieljpc19@gmail.com>
2024-11-27 11:07:38 -0500

    deploy

--- src/marketplace/marketplace/settings.py
+++ src/marketplace/marketplace/settings.py
@@ -27,7 +27,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'danieljpc19@gmail.com'
 EMAIL_HOST_PASSWORD = 'kqfvkjmkuyrnwubm'
@@ -35,7 +35,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_SSL = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["inline-marketplace.onrender.com"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
